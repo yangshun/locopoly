aff4765c88f75f4235c2968246c38563817967f1
haoqiang <mail.haoqiang@gmail.com>
2016-06-25 06:36:09 +0800

    Add messenger callback

--- index.js
+++ index.js
@@ -6,24 +6,66 @@ var controller = require('./api/controller');
 var processor = require('./worker/processor');
 
 
+//
+// Part one, web interface
+//
 var app = express();
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "whatever") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+});
+
+app.listen(5000);
+
+console.log('Express listening on port 5000...');
+
+
+//
+// Part two, monitoring firebase
+//
 var firebaseApp = firebase.initializeApp(config.firebase);
-
-
-//
-// Task one, web interface
-//
-
-//app.use('/api', controller);
-//app.listen(3000);
-//
-//console.log('Express listening on port 3000...');
-
-
-//
-// Task one, monitoring firebase
-//
-
 var database = firebaseApp.database();
 
 var usersRef = database.ref("data/users");
