5c869269b30defed2218b1d1daeb23aa75995512
ianngiaw <ianngiawtingan@gmail.com>
2016-06-25 15:01:34 +0800

    redirect to profile on login

--- web/app/scripts/controllers/login.js
+++ web/app/scripts/controllers/login.js
@@ -7,15 +7,15 @@
  * # LoginCtrl
  * Controller of the locopoly
  */
-angular.module('locopoly').controller('LoginCtrl', ['$scope', 'Auth',
-  function ($scope, Auth) {
+angular.module('locopoly').controller('LoginCtrl', ['$scope', '$location', 'Auth',
+  function ($scope, $location, Auth) {
     $scope.signIn = function() {
       $scope.firebaseUser = null;
       $scope.error = null;
 
       var provider = new firebase.auth.FacebookAuthProvider();
       Auth.$signInWithPopup(provider).then(function(firebaseUser) {
-        $scope.firebaseUser = firebaseUser;
+        $location.path('/profile');
       }).catch(function(error) {
         $scope.error = error;
       });
