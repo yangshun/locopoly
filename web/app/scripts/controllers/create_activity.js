df9aafdc917f21de16a8c59dac19a22091bad41e
ianngiaw <ianngiawtingan@gmail.com>
2016-06-25 21:22:59 +0800

    Update create activity

--- web/app/scripts/controllers/create_activity.js
+++ web/app/scripts/controllers/create_activity.js
@@ -7,14 +7,17 @@
  * # CreateActivityCtrl
  * Controller of the locopoly
  */
+/*
+*/
 angular.module('locopoly')
-  .controller('CreateActivityCtrl', function ($scope) {
+  .controller('CreateActivityCtrl', function ($scope, currentAuth) {
     var defaultActivity = {
-      current_attendees: 0,
-      impressions: 0,
-      hearts: 0
+      views: 0,
+      likes: 0,
+      type: 'games',
+      verified: false,
+      createdAt: Date.now()
     };
-
     $scope.activity = _.assign({}, defaultActivity);
 
     var USE_FAKER = true;
@@ -25,14 +28,19 @@ angular.module('locopoly')
         description: faker.lorem.sentences(),
         address: faker.address.streetAddress(),
         type: _.sample(['games', 'event', 'buying', 'selling']),
-        creator: 558978353,
+        creator: {
+          uid: currentAuth.uid,
+          name: currentAuth.displayName,
+          image: currentAuth.photoURL
+        },
         verified: _.sample([true, false]),
         startTime: moment(fakeDate).add(1, 'day').format('X'),
         endTime: moment(fakeDate).add(_.sample([2, 3, 4], 'day')).format('X'),
         latitude: 1.331892 + _.sample([0.01, 0.005, 0.003, -0.01, -0.005, -0.003]),  // Toa Payoh
         longitude:  103.849388 + _.sample([0.01, 0.005, 0.003, -0.01, -0.005, -0.003]), // Toa Payoh
         cost: faker.finance.amount(),
-        maxAllowed: _.sample([3,5,8,13,21])
+        maxAllowed: _.sample([3,5,8,13,21]),
+        bounty: _.sample([3,5,8,13,21])
       });
     }
 
