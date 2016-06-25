682d58a20da64d822630020de27f25cdea17488e
ianngiaw <ianngiawtingan@gmail.com>
2016-06-25 20:29:15 +0800

    Added activity chat

--- web/app/scripts/controllers/activity.js
+++ web/app/scripts/controllers/activity.js
@@ -8,7 +8,32 @@
  * Controller of the locopoly
  */
 angular.module('locopoly')
-  .controller('ActivityCtrl', function ($scope, $firebaseObject, $routeParams) {
+  .controller('ActivityCtrl', function ($scope, $firebaseObject, $firebaseArray, $routeParams, currentAuth) {
     var ref = firebase.database().ref().child('data').child('activities').child($routeParams.activityId);
     $scope.activity = $firebaseObject(ref);
+
+    var orderedMessagesRef = ref.child('comments').orderByChild('createdAt');
+    $scope.messages = $firebaseArray(orderedMessagesRef);
+
+    var messagesRef = ref.child('comments');
+
+    $scope.message = {
+      text: ''
+    };
+    $scope.sendMessage = function(text) {
+      var newMessageKey = messagesRef.push().key
+      var message = {
+        message: text,
+        createdAt: Date.now(),
+        author: {
+          uid: currentAuth.uid,
+          name: currentAuth.displayName,
+          image: currentAuth.photoURL
+        }
+      };
+      var updates = {};
+      updates[newMessageKey] = message;
+      messagesRef.update(updates);
+      $scope.message.text = '';
+    };
   });
